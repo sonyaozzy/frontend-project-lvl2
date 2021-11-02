6cbd38472135062bc37140e382bb442595832b01
Anna Papiashvili <an.papiashvili@gmail.com>
2021-11-02 17:27:05 +0400

    Second Stage

--- src/help.js
+++ src/help.js
@@ -5,6 +5,9 @@ const showHelp = () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 
   program.parse();
 };
